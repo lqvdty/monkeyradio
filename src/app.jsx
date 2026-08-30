1302f7c2b697afa472020ec440be549b58e42ac7
yesharsha <yes.harshavardhan@gmail.com>
2026-08-30 20:38:33 +0200

    Selector overrides: Ozy Breaks, 100,000 Vibrations, hip hop mixes
    - Sunday Special Ozy Breaks -> Dj Ozon
    - 100,000 Vibrations -> Dakta Dub
    - October Hip Hop Mix feat Lady Rappers / Strictly hip hop mix /
      Summer Hip Hop & RnB Mix -> Dj Def Hawk

--- src/app.jsx
+++ src/app.jsx
@@ -543,7 +543,12 @@ class Component extends React.Component {
     [/\bworld of sound\b/i, 'Dakta Dub'],
     [/world radio day special/i, 'Dakta Dub'],
     [/^\s*dub\s*$/i, 'Dakta Dub'],
-    [/dub'in and step'in/i, 'Dakta Dub']
+    [/dub'in and step'in/i, 'Dakta Dub'],
+    [/sunday special ozy breaks/i, 'Dj Ozon'],
+    [/100,?000 vibrations/i, 'Dakta Dub'],
+    [/october hip hop mix feat lady rappers/i, 'Dj Def Hawk'],
+    [/strictly hip hop mix/i, 'Dj Def Hawk'],
+    [/summer hip hop & rnb mix/i, 'Dj Def Hawk']
   ];
 
   djFrom(raw) {
